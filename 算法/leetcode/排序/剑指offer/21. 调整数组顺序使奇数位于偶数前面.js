69f3ba20a12667f6164eeab1deb6a94cac02a995
muyeyong <xuyongshuaige@gmail.com>
2022-05-06 22:48:31 +0800

    20220506-剑指offer

---
+++ 算法/leetcode/排序/剑指offer/21. 调整数组顺序使奇数位于偶数前面.js
@@ -0,0 +1,24 @@
+// 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数在数组的前半部分，所有偶数在数组的后半部分。
+
+//  
+
+// 示例：
+
+// 输入：nums = [1,2,3,4]
+// 输出：[1,3,2,4] 
+// 注：[3,1,2,4] 也是正确的答案之一。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var exchange = function(nums) {
+  return nums.sort((a, b) => {
+    return b%2 - a%2 
+  })
+};
+
+console.log(exchange([1,2,3,4]))
