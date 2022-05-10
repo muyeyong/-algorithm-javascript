aa1658fa86558fd94c0d089c76b095db1340fec7
muyeyong <xuyongshuaige@gmail.com>
2022-05-10 22:58:27 +0800

    剑指offer-链表倒数k个节点

---
+++ 算法/leetcode/排序/剑指offer/22. 链表中倒数第k个节点.js
@@ -0,0 +1,35 @@
+// 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+
+// 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode.cn/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+ var getKthFromEnd = function(head, k) {
+   if (!head) return head
+  let len = 0
+  let  i = head; let step = 1
+  while(i) {
+    len++
+    i = i.next
+  }
+  i = head
+  while(len - step >= k) {
+    step++
+    i = i.next
+  }
+  return i
+};
